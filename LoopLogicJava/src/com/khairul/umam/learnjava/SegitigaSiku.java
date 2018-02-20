9dd365b43fb2a3faf4abc990cbc934ad059f4b71
ruheru <umamkhairul27@gmail.com>
2018-02-20 22:42:42 +0700

    first change

--- LoopLogicJava/src/com/khairul/umam/learnjava/SegitigaSiku.java
+++ LoopLogicJava/src/com/khairul/umam/learnjava/SegitigaSiku.java
@@ -12,10 +12,25 @@ public class SegitigaSiku {
 			System.out.println();
 		}
 	}
+	
+	public void segitiga2(int n) {
+		System.out.println("segitiga 2: ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if(i <= j) {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		SegitigaSiku ss = new SegitigaSiku();
 		int n = 9;
 		ss.segitiga1(n);
+		System.out.println("==========================");
+		ss.segitiga2(n);
+		
 	}
 }
